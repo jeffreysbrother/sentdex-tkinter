7c52deb4e56f42eb820b2f03e58a71bb81176cb1
James Cool <jeffreysbrother@gmail.com>
2016-08-15 23:31:44 -0700

    add stupid favicon but that shiz didn't work

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ class BLM(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+
+        # tk.Tk.iconbitmap(self, default="favicon.ico")
+
         container = tk.Frame(self)
-
         container.pack(side="top", fill="both", expand=True)
-
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
