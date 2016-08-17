21735048323865049f6c47f69049d8dfdfba7924
James Cool <jeffreysbrother@gmail.com>
2016-08-16 22:18:55 -0700

    matplotlib graph functioning

--- app.py
+++ app.py
@@ -129,6 +129,14 @@ class PageThree(tk.Frame):
                             command=lambda: controller.show_frame(PageTwo))
         button2.pack()
 
+        f = Figure(figsize=(5,5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
 
 
 app = BLM()
