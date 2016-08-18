c389a8d95a97c61486445843a997747e1edc4605
James Cool <jeffreysbrother@gmail.com>
2016-08-17 23:07:31 -0700

    remove superfluous function

--- app.py
+++ app.py
@@ -25,14 +25,11 @@ a = f.add_subplot(111)
 
 def popupmsg(msg):
     popup = tk.Tk()
-
-    def leavemini():
-        popup.destroy()
     
     popup.wm_title("!")
     label = ttk.Label(popup, text=msg, font=NORM_FONT)
     label.pack(side="top", fill="x", pady=10)
-    B1 = ttk.Button(popup, text="Okay", command=leavemini)
+    B1 = ttk.Button(popup, text="Okay", command=popup.destroy)
     B1.pack()
     popup.mainloop()
 
